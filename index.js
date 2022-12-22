638e22ee6676a7d644b283211e6a6b4fb8a43369
AbaidUllah <abaidu408@gmail.com>
2022-12-22 14:51:15 +0500

    frond end complete

---
+++ index.js
@@ -0,0 +1,15 @@
+let setting_button=document.getElementById("setting_button")
+let setting_manu=document.getElementById("setting-menu")
+let apps=document.getElementById("apps")
+let apps_button=document.getElementById("apps_button")
+let dis=document.getElementsByClassName(".none_display")
+setting_button.addEventListener("click",function(even){
+
+    //         
+    
+})
+apps_button.addEventListener("click",function(){
+    apps.classList.toggle("none_display")
+})
+
+
