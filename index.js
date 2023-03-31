ed87ccec21eba11af31fa006d74ed55af8849ae8
AbaidUllah <abaidu408@gmail.com>
2023-03-31 12:40:18 +0500

    Made changes to the div element to hide it when clicking outside

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-let setting_button=document.getElementById("setting_button")
-let setting_manu=document.getElementById("setting-menu")
-let apps=document.getElementById("apps")
-let apps_button=document.getElementById("apps_button")
-let dis=document.getElementsByClassName(".none_display")
-setting_button.addEventListener("click",function(even){
+let setting_button = document.getElementById("setting_button");
+let setting_manu = document.getElementById("setting-menu");
+let apps = document.getElementById("apps");
+let apps_button = document.getElementById("apps_button");
+let dis = document.getElementsByClassName(".none_display");
+document.addEventListener("click", function(event) {
+  if (!apps.contains(event.target) && event.target !== apps_button) {
+    // Hide the div apps
+    apps.style.display = "none";
+  }
+  if (!setting_manu.contains(event.target) && event.target !== apps_button) {
+    // Hide the div setting-manu
+    setting_manu.style.display = "none";
+  }
+});
 
-    //         
-    
-})
-apps_button.addEventListener("click",function(){
-    apps.classList.toggle("none_display")
-})
-
+// Add a click event listener to the show apps button
+apps_button.addEventListener("click", function(event) {
+  // Toggle the display of the div apps
+  apps.style.display = apps.style.display === "none" ? "flex" : "none";
+  // Prevent the click event from bubbling up to the document object
+  event.stopPropagation();
+});
+setting_button.addEventListener("click", function(event) {
+  // Toggle the display of the div apps using ternary operator
+  setting_manu.style.display = apps.style.display === "none" ? "flex" : "none";
+  event.stopPropagation();
+});
 
